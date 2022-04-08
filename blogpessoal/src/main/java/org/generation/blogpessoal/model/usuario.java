55b94fe170fd78577bd409adb283c2e71e23d1e1
rafaelmoreira-lima <rafaelmoreira.lima@hotmail.com>
2022-04-08 19:30:06 -0300

    testes com JUnit

---
+++ blogpessoal/src/main/java/org/generation/blogpessoal/model/usuario.java
@@ -0,0 +1,104 @@
+package org.generation.blogpessoal.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name="tb_usuarios")
+public class usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	
+	@Email
+	private String usuario;
+	
+	@NotNull
+	private String senha;
+	
+	private String foto;
+
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
+	private List<postagem> postagens;
+
+	
+	public List<postagem> getPostagens() {
+		return postagens;
+	}
+
+	public void setPostagens(List<postagem> postagens) {
+		this.postagens = postagens;
+	}
+
+	
+	//METADOS CONSTRUTORES PARA TESTES.
+	//IMPORTANTE SEGUIR A MESMA ORDEM DOS ATRIBUTOS ACIMA.
+	//CONSTRUTOR CHEIO:
+	public usuario(Long id, String nome, String usuario, String senha, String foto) {
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+	}
+	
+	//CONSTRUTOR VAZIO:
+	public usuario() {}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	
+	
+}	
---
+++ blogpessoal/src/main/java/org/generation/blogpessoal/repository/usuarioRepository.java
@@ -0,0 +1,17 @@
+package org.generation.blogpessoal.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.generation.blogpessoal.model.usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface usuarioRepository extends JpaRepository<usuario, Long>{
+
+	public Optional<usuario> findAllByUsuarioContainingIgnoreCase(String usuario);
+	
+	public List<usuario> findAllByNomeContainingIgnoreCase(String nome);
+	
+}
